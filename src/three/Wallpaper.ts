140e36bf63f6bd3e8734e9fe2e716d7d66ecc7be
Trevor Du <trevorguanhao@gmail.com>
2024-09-14 13:10:08 +0200

    waves with phase

--- src/three/Wallpaper.ts
+++ src/three/Wallpaper.ts
@@ -2,16 +2,47 @@ import { Theme } from "@/util/Theme";
 import * as THREE from "three";
 import { disposeMesh } from "@/three/util";
 
+class Wave {
+  constructor(
+    mesh: THREE.Mesh,
+    waveWidth: number,
+    screenWidth: number,
+    x: number
+  ) {
+    this.mesh = mesh;
+    this.waveWidth = waveWidth;
+    this.screenWidth = screenWidth;
+    this.x = 0;
+    this.phaseShift(x);
+  }
+
+  phaseShift(x: number) {
+    this.x += x;
+
+    // wrap wave around if mesh is too far right
+    if (this.x > -1) {
+      this.x = -this.waveWidth / 2 + this.x - 1;
+    }
+
+    this.mesh.position.setX(this.x);
+  }
+
+  mesh: THREE.Mesh;
+  waveWidth: number;
+  screenWidth: number;
+  x: number;
+}
+
 export class Wallpaper {
   constructor() {
-    this.shapes = [];
+    this.waves = [];
   }
 
   generate(scene: THREE.Scene, theme: Theme) {
-    for (const shape of this.shapes) {
-      disposeMesh(shape);
+    for (const wave of this.waves) {
+      disposeMesh(wave.mesh);
     }
-    this.shapes = [];
+    this.waves = [];
 
     const nWaves = theme.wallpaperTheme.nWaves.getInteger();
     const gradientPalette =
@@ -28,14 +59,13 @@ export class Wallpaper {
       const waveShape = new THREE.Shape();
       waveShape.moveTo(0, 0);
 
-      const waveWidth = Math.ceil(2 / period);
-      const nSamples = 100;
+      const waveWidth = Math.ceil(2 / period) * 2;
+      const nSamples = 200;
       for (let i = 0; i < nSamples; ++i) {
         // TODO scale number of samples based on frequency?
         const x = (waveWidth / (nSamples - 1)) * i;
         const y = amplitude * Math.sin(frequency * x);
         waveShape.lineTo(x, y);
-        console.log(x, y);
       }
 
       waveShape.lineTo(waveWidth, -1);
@@ -48,7 +78,7 @@ export class Wallpaper {
       });
       const mesh = new THREE.Mesh(geometry, material);
       mesh.position.set(-1, (2 / nWaves) * (i + 0.5) - 1 + yPerturbation, -i);
-      this.shapes.push(mesh);
+      this.waves.push(new Wave(mesh, waveWidth, 2, -1));
     }
 
     // background
@@ -60,14 +90,14 @@ export class Wallpaper {
     this.background = new THREE.Mesh(bgGeometry, bgMaterial);
     this.background.position.set(0, 0, -100);
 
-    for (const shape of this.shapes) {
-      scene.add(shape);
+    for (const wave of this.waves) {
+      scene.add(wave.mesh);
     }
     if (this.background) {
       scene.add(this.background);
     }
   }
 
-  shapes: THREE.Mesh[];
+  waves: Wave[];
   background?: THREE.Mesh;
 }
